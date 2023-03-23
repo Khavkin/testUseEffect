0b64963ab211c51d12c866eb9e6f30e08356bdf6
Khavkin <hdm@ukr.net>
2023-03-23 18:29:39 +0200

    add code

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,17 @@
+import { useEffect, useState } from 'react';
+
 export const App = () => {
+  const [contacts, setContacts] = useState([]);
+
+  useEffect(() => {
+    const contactsList = JSON.parse(localStorage.getItem('contacts')) ?? [];
+    setContacts(contactsList);
+  }, []); // ComponentDidMount
+
+  useEffect(() => {
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }, [contacts]); // ComponentDidUpdate
+
   return (
     <div
       style={{
@@ -7,7 +20,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
